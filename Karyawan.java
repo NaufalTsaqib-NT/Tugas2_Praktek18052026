ec3a463b2e0eb26a320f15cb1c4cc25632e31c24
Naufal Tsaqib <borncarnival180@gmail.com>
2026-05-19 13:50:08 +0700

    Implement Karyawan class with attributes and methods
    Added properties and methods for Karyawan class.

--- Karyawan.java
+++ Karyawan.java
@@ -3,11 +3,34 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Pratikum18052026.Tugas2;
+//Naufal Tsaqib.    2501082009
 
-/**
- *
- * @author LAB-SI-PC
- */
-public class Karyawan {
-    
+public abstract class Karyawan {
+    protected String nama;
+    protected String idKaryawan;
+
+    public Karyawan(String nama, String idKaryawan) {
+        this.nama = nama;
+        this.idKaryawan = idKaryawan;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getIdKaryawan() {
+        return idKaryawan;
+    }
+
+    public void setIdKaryawan(String idKaryawan) {
+        this.idKaryawan = idKaryawan;
+    }
+
+    public abstract double hitungGajiBulanIni();
+    public abstract void tampilkanDetail();
 }
+
